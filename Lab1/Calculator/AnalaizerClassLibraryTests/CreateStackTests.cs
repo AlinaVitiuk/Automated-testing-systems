234ee87d950a9a5721bfd47460c0234849e20ab9
AlinaVityuk <alina.vitiuk.pm.2020@lpnu.ua>
2022-09-26 23:03:11 +0200

    Lab1

---
+++ Lab1/Calculator/AnalaizerClassLibraryTests/CreateStackTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using AnalaizerClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnalaizerClassLibraryTests
+{
+    [TestClass]
+    public class CreateStackTests
+    {
+
+        public TestContext TestContext { get; set; }
+
+        [DataSource("System.Data.SqlClient", @"Data Source=(LocalDB)\MSSQLLocalDB;Database=TestData;Trusted_Connection=True;MultipleActiveResultSets=true", "TestingData", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void CreateStack_FromDataSourceTest()
+        {
+            // Arrange
+            string initial = (string)TestContext.DataRow["actual"];
+            string expected = (string)TestContext.DataRow["expected"];
+            AnalaizerClass.expression = initial;
+
+            // Actual
+            string actual = string.Join("", AnalaizerClass.CreateStack().ToArray());
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Lab1/Calculator/AnalaizerClassLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalaizerClassLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalaizerClassLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5345e317-8d25-418a-8806-1a9e881ab000")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
